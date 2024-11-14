0a5d786a81b3591653b1b2867924ea8515e2db94
Valdir Júnior <valdirmsjunior@gmail.com>
2024-11-13 21:11:03 -0300

    criando seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,10 +16,15 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $superAdminRole = Role::firstOrCreate(['name' => 'super-admin']);
+        $managerRole = Role::firstOrCreate(['name' => 'manager']);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory()->count(10)->create()->each(function($user) use ($superAdminRole, $managerRole)
+        {
+            $roleID = $user->id % 2 == 0 ? $superAdminRole : $managerRole;
+            $user->Roles()->attach($roleID);
+        });
+
+        Post::factory(30)->create();
     }
 }
